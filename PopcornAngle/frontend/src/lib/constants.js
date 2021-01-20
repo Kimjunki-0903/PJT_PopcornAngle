17c57fdcdf93587e4e6c74d41abaee6fd5b52754
kimjunki <wemanse@naver.com>
2021-01-20 20:03:39 +0900

    PopcornAngle project

---
+++ PopcornAngle/frontend/src/lib/constants.js
@@ -0,0 +1,15 @@
+export default {
+    TITLE: "영화 사이트",
+    // BaseUrl:"",
+    SERVER_URL: "",
+    URL_TYPE: {
+        MAIN: "main",
+        USER: {
+            SIGNUP: "signup",
+            PROFILE: "profile",
+        },
+        MOVIE: {
+            DETAIL: "detail",
+        },
+    }
+}
